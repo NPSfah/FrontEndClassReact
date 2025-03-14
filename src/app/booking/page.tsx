151b55cd284357b25c31a00a17d4ffd653496652
NPSfah <84497623+NPSfah@users.noreply.github.com>
2025-03-14 23:46:27 +0700

    subsission1

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,7 +1,25 @@
+import DatePicker from "@/components/DateReserve";
+import { TextField,Select,MenuItem,InputLabel } from "@mui/material";
 export default function Booking() {
     return (
-        <div>
-            <h1>Venue Booking</h1>
-        </div>
+        <main className="w-[100%] flex flex-col items-center space-y-5">
+            <div className="text-xl font-bold">New Reservation</div>
+
+            <div className="w-fit space-y-2 flex flex-col items-left">
+                <TextField id="Name-Lastname" label="Name-Lastname" name="Name-Lastname" variant="outlined" />
+                <TextField id="Contact-Number" label="Contact-Number" name="Contact-Number" variant="outlined" />
+                <InputLabel id="venue-label">Select Venue</InputLabel>
+                <Select id="venue" variant="outlined" name="venue" label="Venue">
+                    <MenuItem value="Bloom">The Bloom Pavilion</MenuItem>
+                    <MenuItem value="Spark">Spark Space</MenuItem>
+                    <MenuItem value="GrandTable">The Grand Table</MenuItem>
+                </Select>
+                <DatePicker />
+                <button className="rounded-md bg-red-500 hover:bg-red-700 px-4 py-2 text-white">
+                    Book Venue
+                </button>
+            </div>
+
+        </main>
     );
 }
